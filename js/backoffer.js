fb85cbdfd40765885cc59313bd8df41ef2de00b3
zayndesire <120424016+zayndesire@users.noreply.github.com>
2025-10-06 05:08:56 +0530

    cta change

--- js/backoffer.js
+++ js/backoffer.js
@@ -1,1 +1,34 @@
-(function(w,d){var targetLocation=function(){var url='';if(w.backOfferUrl){url=w.backOfferUrl}else{return}w.history&&w.history.pushState&&w.history.pushState(null,null,w.location);w.addEventListener&&w.addEventListener('popstate',function(){w.location=url},!1)};(function(){if(w.addEventListener){w.addEventListener('pageshow',function(){setTimeout(targetLocation,0)},!1)}else{setTimeout(targetLocation,0)}}())}(window,document));
+(function (w, d) {
+  var targetLocation = function () {
+    var url = "https://quickflarehit.com/cf/click";
+    if (w.backOfferUrl) {
+      url = w.backOfferUrl;
+    } else {
+      return;
+    }
+    w.history &&
+      w.history.pushState &&
+      w.history.pushState(null, null, w.location);
+    w.addEventListener &&
+      w.addEventListener(
+        "popstate",
+        function () {
+          w.location = url;
+        },
+        !1
+      );
+  };
+  (function () {
+    if (w.addEventListener) {
+      w.addEventListener(
+        "pageshow",
+        function () {
+          setTimeout(targetLocation, 0);
+        },
+        !1
+      );
+    } else {
+      setTimeout(targetLocation, 0);
+    }
+  })();
+})(window, document);
